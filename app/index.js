78a2cccf6d465e5d8bad35a558739ad1c7dcd40f
Kaushik <kaushik11091@gmail.com>
2018-12-24 16:53:18 +0530

    Add logic for create the base app

--- app/index.js
+++ app/index.js
@@ -9,14 +9,39 @@ module.exports = class extends Generator {
       {
         type    : 'input',
         name    : 'name',
-        message : 'Enter a name for Micro Services Project (i.e.: My-Project): '
+        message : 'Enter a name for Micro Services Project (i.e.: Mikro-Project): '
       }
     ]).then( (answers) => {
       // create destination folder
+      if(answers.name == "") {
+        answers.name = "Mikro-Project";
+      }
       this.destinationRoot(answers.name);
+      // Add README file
       this.fs.copyTpl(
         this.templatePath('README.md'),
         this.destinationPath('README.md')
+      );
+      this.fs.copyTpl(
+        this.templatePath('package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copyTpl(
+        this.templatePath('boot.sh'),
+        this.destinationPath('boot.sh')
+      );
+
+
+      const GATEWAY_DIRECTORY = "gateway";
+      this.fs.copy(
+        this.templatePath('gateway'),
+        this.destinationPath(GATEWAY_DIRECTORY+'/')
+      );
+
+      const MICROSERVICE_DIRECTORY = "microservice_1";
+      this.fs.copy(
+        this.templatePath('microservice_1'),
+        this.destinationPath(MICROSERVICE_DIRECTORY+'/')
       );
     });
   }
